03e67a9b471359707a4bc68668bac258c5bf9e15
Philip Ross <rosspa@rose-hulman.edu>
2016-01-20 15:05:11 +0000

    print string works

--- m2/kernel.c
+++ m2/kernel.c
@@ -2,33 +2,30 @@
 team 1-F
 */
 
-int videoMemoryStart = 0xB8000;
-int currentAddress = 0x8000;
-int color = 0x7;
+int PRINT_INTERRUPT = 0x10;
+char ah = 0xe;
 
-void writeString(char str[]);
-void writeChar(char c);
+void printString(char str[]);
+void printChar(char c);
 int main() {
-    writeString("Hello World!\0");
+    printString("Hello World!\0");
 
     while (1) {}
     return 0;
 }
 
-void writeString(char* str) {
+void printString(char* str) {
     int index = 0;
     while (1) {
         if (str[index] == '\0') {
             break;
         }
-        writeChar(str[index]);
+        printChar(str[index]);
         index++;
     }
 }
 
-void writeChar(char c) {
-    putInMemory(0xB000, currentAddress, c);
-    putInMemory(0xB000, currentAddress + 1, color);
-    currentAddress += 2;
+void printChar(char c) {
+    interrupt(PRINT_INTERRUPT, ah * 256 + c, 0, 0, 0);
 }
 
