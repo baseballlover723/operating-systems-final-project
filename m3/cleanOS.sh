be5afb5ddcc9b70d2cef3bbc8e6691020c020db3
Philip Ross <rosspa@rose-hulman.edu>
2016-01-26 23:24:22 +0000

    start m3

---
+++ m3/cleanOS.sh
@@ -0,0 +1,8 @@
+#Team Members: Morgan Cook, Josh Green, Philip Ross
+#Team Number: 1-F
+#This file acts as a 'make clean' command, removing extraneous files created by ./compileOS.sh
+rm bootload
+rm kernel
+rm kernel_asm.o
+rm kernel.o
+rm bochsout.txt
---
+++ m3/kernel.c
@@ -0,0 +1,127 @@
+/* Morgan Cook, Josh Green, Philip Ross Milestone 2
+   team 1-F
+   */
+
+int PRINT_INTERRUPT = 0x10;
+int READ_CHAR_INTERRUPT = 0x16;
+int MAX_BUFFER_SIZE = 80;
+
+void printString(char str[]);
+void printChar(char c);
+int readChar();
+void readString(char str[]);
+void readSector(char* buffer, int sector);
+int mod(a, b);
+int div(a, b);
+void newline();
+void handleInterrupt21(int ax, int bx, int cx, int dx);
+
+int main() {
+    char line[80];
+    char buffer[512];
+    printString("Hello World!");
+    
+    newline();
+    
+    printString("Enter a line: ");
+    readString(line);
+    printString(line);
+    
+    newline(); 
+    
+    readSector(buffer, 30);
+    printString(buffer);
+    
+    newline();
+
+    handleInterrupt21(0, 0, 0, 0);
+    interrupt(0x21, 0, 0, 0, 0);
+
+    newline();
+
+    printString("End of program");
+    while (1) {}
+    return 0;
+}
+
+void newline() {
+    printString("\n****\n\n");
+} 
+
+void printString(char* str) {
+    int index = 0;
+    while (1) {
+        if (str[index] == '\0') {
+            break;
+        } else if (str[index] == '\n') {
+            printChar(0xa);
+            printChar(0xd);
+        } else {
+            printChar(str[index]);
+        }
+        index++;
+        
+    }
+    return;
+}
+
+void printChar(char c) {
+    interrupt(PRINT_INTERRUPT, 0xe * 256 + c, 0, 0, 0);
+}
+
+int readChar() {
+    return interrupt(READ_CHAR_INTERRUPT, 0, 0, 0, 0);
+}
+
+void readString(char str[]) {
+    int index = 0;
+    while (1) {
+        int readCharChar = readChar();
+        printChar(readCharChar);
+        if (readCharChar == 0x8) {
+            printChar(' ');
+            printChar(readCharChar);
+            str[index] = '\0';
+            if (index > 0) {
+                index--;
+            }
+        } else {
+            str[index] = readCharChar;
+            index++;
+        }
+        if (index > MAX_BUFFER_SIZE - 2 || readCharChar == 0xd) {
+            str[index] = 0xa;
+            str[index + 1] = 0x0;
+            printString("\n");
+            break;
+        }
+    }
+}
+
+int mod(int a, int b) {
+    while (a >= b) {
+        a = a - b;
+    }
+    return a;
+}
+
+int div(a, b) {
+    int quotient = 0;
+    while ((quotient  + 1)* b  <= a) {
+        quotient  = quotient  + 1;
+    }
+    return quotient;
+}
+
+void readSector(char* buffer, int sector) {
+    int ax = 2 * 256 + 1;
+    int bx = buffer;
+    int cx = div(sector, 36) * 256 + mod(sector, 18) + 1;
+    int dx = mod(div(sector, 18), 2) * 256;
+    interrupt(0x13, ax, bx, cx, dx);
+}
+
+void handleInterrupt21(int ax, int bx, int cx, int dx) {
+    printString("This is interrupt handler 21");
+}
+
